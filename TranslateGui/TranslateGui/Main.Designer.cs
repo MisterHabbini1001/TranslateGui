868d2517bc42007eecadf61468b50e6cb0f92ea1
MisterHabbini1001 <0910681@hr.nl>
2017-09-25 14:50:43 +0200

    Modified Main.cs

--- TranslateGui/TranslateGui/Main.Designer.cs
+++ TranslateGui/TranslateGui/Main.Designer.cs
@@ -28,11 +28,78 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.EnterTextLabel = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.LanguageLabel = new System.Windows.Forms.Label();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // EnterTextLabel
+            // 
+            this.EnterTextLabel.AutoSize = true;
+            this.EnterTextLabel.Location = new System.Drawing.Point(25, 23);
+            this.EnterTextLabel.Name = "EnterTextLabel";
+            this.EnterTextLabel.Size = new System.Drawing.Size(52, 13);
+            this.EnterTextLabel.TabIndex = 0;
+            this.EnterTextLabel.Text = "Enter text";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(28, 52);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(790, 176);
+            this.textBox1.TabIndex = 1;
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(152, 274);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(121, 21);
+            this.comboBox1.TabIndex = 2;
+            // 
+            // LanguageLabel
+            // 
+            this.LanguageLabel.AutoSize = true;
+            this.LanguageLabel.Location = new System.Drawing.Point(25, 274);
+            this.LanguageLabel.Name = "LanguageLabel";
+            this.LanguageLabel.Size = new System.Drawing.Size(55, 13);
+            this.LanguageLabel.TabIndex = 3;
+            this.LanguageLabel.Text = "Language";
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(28, 325);
+            this.textBox2.Multiline = true;
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(790, 236);
+            this.textBox2.TabIndex = 4;
+            // 
+            // Main
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1031, 702);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.LanguageLabel);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.EnterTextLabel);
+            this.Name = "Main";
             this.Text = "Main";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Label EnterTextLabel;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Label LanguageLabel;
+        private System.Windows.Forms.TextBox textBox2;
     }
 }
